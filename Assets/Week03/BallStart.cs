56b2c308e1de6c84630b2664d56f570b2fe08fe0
ParisStacy <parispluto@gmail.com>
2020-09-20 11:35:04 -0700

    Week03Project

---
+++ Assets/Week03/BallStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStart : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+        
+    }
+}
---
+++ Assets/Week03/TriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    [SerializeField]
+    private Camera newCam;
+    Camera current;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Switch");
+        current = Camera.current;
+        current.gameObject.active = false;
+        newCam.gameObject.active = true;
+    }
+}
